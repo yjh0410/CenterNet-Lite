6ba2a5ace14ef53f69697fdc1149ac7305b9dfcb
yjh0410 <1394571815@qq.com>
2021-01-24 12:02:30 +0800

    update

---
+++ demo.py
@@ -0,0 +1,198 @@
+import argparse
+import os
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from data import BaseTransform, VOC_CLASSES, coco_class_index, coco_class_labels
+from data import config
+import numpy as np
+import cv2
+import tools
+import time
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='CenterNet Demo Detection')
+
+    parser.add_argument('-v', '--version', default='centernet',
+                        help='centernet.')
+    parser.add_argument('--trained_model', default='weights/',
+                        type=str, help='Trained state_dict file path to open')
+    parser.add_argument('--mode', default='image',
+                        type=str, help='Use the data from image, video or camera')
+    parser.add_argument('-size', '--input_size', default=512, type=int,
+                        help='input_size')
+    parser.add_argument('--cuda', action='store_true', default=False,
+                        help='Use cuda')
+    parser.add_argument('--conf_thresh', default=0.3, type=float,
+                        help='Confidence threshold')
+    parser.add_argument('--nms_thresh', default=0.45, type=float,
+                        help='NMS threshold')
+    parser.add_argument('--path_to_img', default='data/demo/Images/',
+                        type=str, help='The path to image files')
+    parser.add_argument('--path_to_vid', default='data/demo/video/',
+                        type=str, help='The path to video files')
+    parser.add_argument('--path_to_saveVid', default='data/video/result.avi',
+                        type=str, help='The path to save the detection results video')
+    parser.add_argument('-vs', '--visual_threshold', default=0.3,
+                        type=float, help='visual threshold')
+    parser.add_argument('-nms', '--use_nms', action='store_true', default=False,
+                        help='use nms.')
+    
+    return parser.parse_args()
+                    
+
+def vis(img, bbox_pred, scores, cls_inds, class_color, thresh=0.3):
+        
+    for i, box in enumerate(bbox_pred):
+        if scores[i] > thresh:
+            cls_indx = cls_inds[i]
+            cls_id = coco_class_index[int(cls_indx)]
+            cls_name = coco_class_labels[cls_id]
+            mess = '%s: %.3f' % (cls_name, scores[i])
+            # bounding box
+            xmin, ymin, xmax, ymax = box
+            box_w = int(xmax - xmin)
+            cv2.rectangle(img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), class_color[int(cls_indx)], 2)
+            cv2.rectangle(img, (int(xmin), int(abs(ymin)-15)), (int(xmin+box_w*0.55), int(ymin)), class_color[int(cls_indx)], -1)
+
+            cv2.putText(img, mess, (int(xmin), int(ymin)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
+    return img
+
+
+def detect(net, device, transform, thresh, mode='image', path_to_img=None, path_to_vid=None, path_to_save=None):
+    class_color = [(np.random.randint(255),np.random.randint(255),np.random.randint(255)) for _ in range(80)]
+    # ------------------------- Camera ----------------------------
+    # I'm not sure whether this 'camera' mode works ...
+    if mode == 'camera':
+        print('use camera !!!')
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        while True:
+            ret, frame = cap.read()
+            cv2.imshow('current frame', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+            x = torch.from_numpy(transform(frame)[0][:, :, (2, 1, 0)]).permute(2, 0, 1)
+            x = x.unsqueeze(0).to(device)
+
+            torch.cuda.synchronize()
+            t0 = time.time()
+            detections = net(x)      # forward pass
+            torch.cuda.synchronize()
+            t1 = time.time()
+            print("detection time used ", t1-t0, "s")
+            # scale each detection back up to the image
+            scale = np.array([[frame.shape[1], frame.shape[0],
+                                frame.shape[1], frame.shape[0]]])
+            bbox_pred, scores, cls_inds = detections
+            # map the boxes to origin image scale
+            bbox_pred *= scale
+
+            frame_processed = vis(frame, bbox_pred, scores, cls_inds, class_color, setup, thresh=thresh)
+            cv2.imshow('detection result', frame_processed)
+            cv2.waitKey(1)
+        cap.release()
+        cv2.destroyAllWindows()
+
+    # ------------------------- Image ----------------------------
+    elif mode == 'image':
+        for file in os.listdir(path_to_img):
+            img = cv2.imread(path_to_img + '/' + file, cv2.IMREAD_COLOR)
+            x = torch.from_numpy(transform(img)[0][:, :, (2, 1, 0)]).permute(2, 0, 1)
+            x = x.unsqueeze(0).to(device)
+
+            torch.cuda.synchronize()
+            t0 = time.time()
+            detections = net(x)      # forward pass
+            torch.cuda.synchronize()
+            t1 = time.time()
+            print("detection time used ", t1-t0, "s")
+            # scale each detection back up to the image
+            scale = np.array([[img.shape[1], img.shape[0],
+                                img.shape[1], img.shape[0]]])
+            bbox_pred, scores, cls_inds = detections
+            # map the boxes to origin image scale
+            bbox_pred *= scale
+
+            img_processed = vis(img, bbox_pred, scores, cls_inds, class_color=class_color, setup=setup, thresh=thresh)
+            cv2.imshow('detection result', img_processed)
+            cv2.waitKey(0)
+
+    # ------------------------- Video ---------------------------
+    elif mode == 'video':
+        video = cv2.VideoCapture(path_to_vid)
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('output000.avi',fourcc, 40.0, (1280,720))
+        while(True):
+            ret, frame = video.read()
+            
+            if ret:
+                # ------------------------- Detection ---------------------------
+                t0 = time.time()
+                x = torch.from_numpy(transform(frame)[0][:, :, (2, 1, 0)]).permute(2, 0, 1)
+                x = x.unsqueeze(0).to(device)
+
+                torch.cuda.synchronize()
+                t0 = time.time()
+                detections = net(x)      # forward pass
+                torch.cuda.synchronize()
+                t1 = time.time()
+                print("detection time used ", t1-t0, "s")
+                # scale each detection back up to the image
+                scale = np.array([[frame.shape[1], frame.shape[0],
+                                    frame.shape[1], frame.shape[0]]])
+                bbox_pred, scores, cls_inds = detections
+                # map the boxes to origin image scale
+                bbox_pred *= scale
+                
+                frame_processed = vis(frame, bbox_pred, scores, cls_inds, class_color=class_color, setup=setup, thresh=thresh)
+                out.write(frame_processed)
+                cv2.imshow('detection result', frame_processed)
+                cv2.waitKey(1)
+            else:
+                break
+        video.release()
+        out.release()
+        cv2.destroyAllWindows()
+
+
+def run():
+    args = parse_args()
+
+    # use cuda
+    if args.cuda:
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    input_size = [args.input_size, args.input_size]
+
+    # load net
+    if args.version == 'centernet':
+        from models.centernet import CenterNet
+        net = CenterNet(device, 
+                        input_size=input_size, 
+                        num_classes=80, 
+                        conf_thresh=args.conf_thresh, 
+                        nms_thresh=args.nms_thresh, 
+                        use_nms=args.use_nms)
+
+    net.load_state_dict(torch.load(args.trained_model, map_location='cuda'))
+    net.to(device).eval()
+    print('Finished loading model!')
+
+    # run
+    if args.mode == 'camera':
+        detect(net, device, BaseTransform(net.input_size), 
+                    thresh=args.visual_threshold, mode=args.mode)
+    elif args.mode == 'image':
+        detect(net, device, BaseTransform(net.input_size), 
+                    thresh=args.visual_threshold, mode=args.mode, path_to_img=args.path_to_img)
+    elif args.mode == 'video':
+        detect(net, device, BaseTransform(net.input_size),
+                    thresh=args.visual_threshold, mode=args.mode, path_to_vid=args.path_to_vid, path_to_save=args.path_to_saveVid)
+
+
+if __name__ == '__main__':
+    run()
---
+++ train.py
@@ -0,0 +1,298 @@
+from __future__ import division
+
+import os
+import random
+import argparse
+import time
+import math
+import numpy as np
+
+import torch
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+
+from data import *
+import tools
+
+from utils.augmentations import SSDAugmentation
+from utils.cocoapi_evaluator import COCOAPIEvaluator
+from utils.vocapi_evaluator import VOCAPIEvaluator
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='CenterNet Detection')
+    parser.add_argument('-v', '--version', default='centernet',
+                        help='centernet')
+    parser.add_argument('-d', '--dataset', default='voc',
+                        help='voc or coco')
+    parser.add_argument('-ms', '--multi_scale', action='store_true', default=False,
+                        help='use multi-scale trick')                  
+    parser.add_argument('--batch_size', default=32, type=int, 
+                        help='Batch size for training')
+    parser.add_argument('--lr', default=1e-3, type=float, 
+                        help='initial learning rate')
+    parser.add_argument('-cos', '--cos', action='store_true', default=False,
+                        help='use cos lr')
+    parser.add_argument('-no_wp', '--no_warm_up', action='store_true', default=False,
+                        help='yes or no to choose using warmup strategy to train')
+    parser.add_argument('--wp_epoch', type=int, default=2,
+                        help='The upper bound of warm-up')
+    parser.add_argument('--start_epoch', type=int, default=0,
+                        help='start epoch to train')
+    parser.add_argument('-r', '--resume', default=None, type=str, 
+                        help='keep training')
+    parser.add_argument('--momentum', default=0.9, type=float, 
+                        help='Momentum value for optim')
+    parser.add_argument('--weight_decay', default=5e-4, type=float, 
+                        help='Weight decay for SGD')
+    parser.add_argument('--gamma', default=0.1, type=float, 
+                        help='Gamma update for SGD')
+    parser.add_argument('--num_workers', default=8, type=int, 
+                        help='Number of workers used in dataloading')
+    parser.add_argument('--eval_epoch', type=int,
+                            default=10, help='interval between evaluations')
+    parser.add_argument('--cuda', action='store_true', default=False,
+                        help='use cuda.')
+    parser.add_argument('--mosaic', action='store_true', default=False,
+                        help='use mosaic augmentation.')
+    parser.add_argument('--tfboard', action='store_true', default=False,
+                        help='use tensorboard')
+    parser.add_argument('--debug', action='store_true', default=False,
+                        help='debug mode where only one image is trained')
+    parser.add_argument('--save_folder', default='weights/', type=str, 
+                        help='Gamma update for SGD')
+
+    return parser.parse_args()
+
+
+def train():
+    args = parse_args()
+
+    path_to_save = os.path.join(args.save_folder, args.dataset, args.version)
+    os.makedirs(path_to_save, exist_ok=True)
+
+    # cuda
+    if args.cuda:
+        print('use cuda')
+        cudnn.benchmark = True
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    # mosaic augmentation
+    if args.mosaic:
+        print('use Mosaic Augmentation ...')
+
+    # multi-scale
+    if args.multi_scale:
+        print('use the multi-scale trick ...')
+        train_size = [640, 640]
+        val_size = [512, 512]
+    else:
+        train_size = [512, 512]
+        val_size = [512, 512]
+
+    cfg = train_cfg
+    # dataset and evaluator
+    print("Setting Arguments.. : ", args)
+    print("----------------------------------------------------------")
+    print('Loading the dataset...')
+
+    if args.dataset == 'voc':
+        data_dir = VOC_ROOT
+        num_classes = 20
+        dataset = VOCDetection(root=data_dir, img_size=train_size[0],
+                                transform=SSDAugmentation(train_size),
+                                mosaic=args.mosaic
+                                )
+
+        evaluator = VOCAPIEvaluator(data_root=data_dir,
+                                    img_size=val_size,
+                                    device=device,
+                                    transform=BaseTransform(val_size),
+                                    labelmap=VOC_CLASSES
+                                    )
+
+    elif args.dataset == 'coco':
+        data_dir = coco_root
+        num_classes = 80
+        dataset = COCODataset(
+                    data_dir=data_dir,
+                    img_size=train_size[0],
+                    transform=SSDAugmentation(train_size),
+                    debug=args.debug,
+                    mosaic=args.mosaic
+                    )
+
+
+        evaluator = COCOAPIEvaluator(
+                        data_dir=data_dir,
+                        img_size=val_size,
+                        device=device,
+                        transform=BaseTransform(val_size)
+                        )
+    
+    else:
+        print('unknow dataset !! Only support voc and coco !!')
+        exit(0)
+    
+    print('Training model on:', dataset.name)
+    print('The dataset size:', len(dataset))
+    print("----------------------------------------------------------")
+
+    # dataloader
+    dataloader = torch.utils.data.DataLoader(
+                    dataset, 
+                    batch_size=args.batch_size, 
+                    shuffle=True, 
+                    collate_fn=detection_collate,
+                    num_workers=args.num_workers,
+                    pin_memory=True
+                    )
+
+    # build model
+    if args.version == 'centernet':
+        from models.centernet import CenterNet
+        
+        net = CenterNet(device, input_size=train_size, num_classes=num_classes, trainable=True)
+        print('Let us train centernet on the %s dataset ......' % (args.dataset))
+
+    else:
+        print('Unknown version !!!')
+        exit()
+
+    model = net
+    model.to(device).train()
+
+    # use tfboard
+    if args.tfboard:
+        print('use tensorboard')
+        from torch.utils.tensorboard import SummaryWriter
+        c_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        log_path = os.path.join('log/coco/', args.version, c_time)
+        os.makedirs(log_path, exist_ok=True)
+
+        writer = SummaryWriter(log_path)
+    
+    # keep training
+    if args.resume is not None:
+        print('keep training model: %s' % (args.resume))
+        model.load_state_dict(torch.load(args.resume, map_location=device))
+
+    # optimizer setup
+    base_lr = args.lr
+    tmp_lr = base_lr
+    optimizer = optim.SGD(model.parameters(), 
+                            lr=args.lr, 
+                            momentum=args.momentum,
+                            weight_decay=args.weight_decay
+                            )
+
+    max_epoch = cfg['max_epoch']
+    epoch_size = len(dataset) // args.batch_size
+
+    # start training loop
+    t0 = time.time()
+
+    for epoch in range(args.start_epoch, max_epoch):
+
+        # use cos lr
+        if args.cos and epoch > 20 and epoch <= max_epoch - 20:
+            # use cos lr
+            tmp_lr = 0.00001 + 0.5*(base_lr-0.00001)*(1+math.cos(math.pi*(epoch-20)*1./ (max_epoch-20)))
+            set_lr(optimizer, tmp_lr)
+
+        elif args.cos and epoch > max_epoch - 20:
+            tmp_lr = 0.00001
+            set_lr(optimizer, tmp_lr)
+        
+        # use step lr
+        else:
+            if epoch in cfg['lr_epoch']:
+                tmp_lr = tmp_lr * 0.1
+                set_lr(optimizer, tmp_lr)
+    
+
+        for iter_i, (images, targets) in enumerate(dataloader):
+            # WarmUp strategy for learning rate
+            if not args.no_warm_up:
+                if epoch < args.wp_epoch:
+                    tmp_lr = base_lr * pow((iter_i+epoch*epoch_size)*1. / (args.wp_epoch*epoch_size), 4)
+                    # tmp_lr = 1e-6 + (base_lr-1e-6) * (iter_i+epoch*epoch_size) / (epoch_size * (args.wp_epoch))
+                    set_lr(optimizer, tmp_lr)
+
+                elif epoch == args.wp_epoch and iter_i == 0:
+                    tmp_lr = base_lr
+                    set_lr(optimizer, tmp_lr)
+        
+            # to device
+            images = images.to(device)
+
+            # multi-scale trick
+            if iter_i % 10 == 0 and iter_i > 0 and args.multi_scale:
+                # randomly choose a new size
+                size = random.randint(10, 19) * 32
+                train_size = [size, size]
+                model.set_grid(train_size)
+            if args.multi_scale:
+                # interpolate
+                images = torch.nn.functional.interpolate(images, size=train_size, mode='bilinear', align_corners=False)
+            
+            # make train label
+            targets = [label.tolist() for label in targets]
+            targets = tools.gt_creator(train_size, net.stride, args.num_classes, targets)
+            targets = torch.tensor(targets).float().to(device)
+
+            # forward and loss
+            cls_loss, txty_loss, twth_loss, total_loss = model(images, target=targets)
+
+            # backprop
+            total_loss.backward()        
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if iter_i % 10 == 0:
+                if args.tfboard:
+                    # viz loss
+                    writer.add_scalar('class loss', cls_loss.item(), iter_i + epoch * epoch_size)
+                    writer.add_scalar('txty loss',  txty_loss.item(), iter_i + epoch * epoch_size)
+                    writer.add_scalar('twth loss',  twth_loss.item(), iter_i + epoch * epoch_size)
+                    writer.add_scalar('total loss', total_loss.item(), iter_i + epoch * epoch_size)
+                
+                t1 = time.time()
+                print('[Epoch %d/%d][Iter %d/%d][lr %.6f]'
+                    '[Loss: cls %.2f || txty %.2f || twth %.2f ||total %.2f || size %d || time: %.2f]'
+                        % (epoch+1, max_epoch, iter_i, epoch_size, tmp_lr,
+                            cls_loss.item(), txty_loss.item(), twth_loss.item(), total_loss.item(), train_size[0], t1-t0),
+                        flush=True)
+
+                t0 = time.time()
+
+        # evaluation
+        if (epoch) % args.eval_epoch == 0:
+            model.trainable = False
+            model.set_grid(val_size)
+            model.eval()
+
+            # evaluate
+            evaluator.evaluate(model)
+
+            # convert to training mode.
+            model.trainable = True
+            model.set_grid(train_size)
+            model.train()
+
+        # save model
+        if (epoch + 1) % 10 == 0:
+            print('Saving state, epoch:', epoch + 1)
+            torch.save(model.state_dict(), os.path.join(path_to_save, 
+                        args.version + '_' + repr(epoch + 1) + '.pth')
+                        )  
+
+
+def set_lr(optimizer, lr):
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+if __name__ == '__main__':
+    train()
