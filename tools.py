35a0e51f836868a83a377efab6524425decfa622
Yonghye Kwon <developer.0hye@gmail.com>
2021-03-22 22:46:22 +0900

    fix gaussian_radius function
    fix #12

--- tools.py
+++ tools.py
@@ -53,25 +53,25 @@ class FocalLoss(nn.Module):
 
 
 def gaussian_radius(det_size, min_overlap=0.7):
-    box_h, box_h  = det_size
-    a1 = 1
-    b1 = (box_h + box_h)
-    c1 = box_h * box_h * (1 - min_overlap) / (1 + min_overlap)
+    box_h, box_w = det_size
+
+    a1  = 1
+    b1  = (box_h + box_w)
+    c1  = box_w * box_h * (1 - min_overlap) / (1 + min_overlap)
     sq1 = np.sqrt(b1 ** 2 - 4 * a1 * c1)
-    r1 = (b1 + sq1) / 2 #(2*a1)
+    r1  = (b1 + sq1) / 2
 
-    a2 = 4
-    b2 = 2 * (box_h + box_h)
-    c2 = (1 - min_overlap) * box_h * box_h
+    a2  = 4
+    b2  = 2 * (box_h + box_w)
+    c2  = (1 - min_overlap) * box_w * box_h
     sq2 = np.sqrt(b2 ** 2 - 4 * a2 * c2)
-    r2 = (b2 + sq2) / 2 #(2*a2)
+    r2  = (b2 + sq2) / 2
 
-    a3 = 4 * min_overlap
-    b3 = -2 * min_overlap * (box_h + box_h)
-    c3 = (min_overlap - 1) * box_h * box_h
+    a3  = 4 * min_overlap
+    b3  = -2 * min_overlap * (box_h + box_w)
+    c3  = (min_overlap - 1) * box_w * box_h
     sq3 = np.sqrt(b3 ** 2 - 4 * a3 * c3)
-    r3 = (b3 + sq3) / 2 #(2*a3)
-
+    r3  = (b3 + sq3) / 2
     return min(r1, r2, r3)
 
 
